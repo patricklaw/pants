1cefa2d38618b1168f370fdc139c1b5adc708749
Patrick Lawson <pl@foursquare.com>
2014-08-14 14:04:48 -0400

    Very minimal tags support

--- src/python/pants/base/target.py
+++ src/python/pants/base/target.py
@@ -166,7 +166,7 @@ class Target(AbstractTarget):
     ids = list(ids)  # We can't len a generator.
     return ids[0] if len(ids) == 1 else cls.combine_ids(ids)
 
-  def __init__(self, name, address, build_graph, payload=None, exclusives=None):
+  def __init__(self, name, address, build_graph, payload=None, exclusives=None, tags=None):
     """
     :param string name: The target name.
     :param Address address: The Address that maps to this Target in the BuildGraph
@@ -175,6 +175,7 @@ class Target(AbstractTarget):
     self.name = name
     self.address = address
     self.payload = payload or EmptyPayload()
+    self._tags = frozenset(tags or [])
     self._build_graph = build_graph
     self.description = None
     self.labels = set()
@@ -186,6 +187,10 @@ class Target(AbstractTarget):
 
     self._cached_fingerprint_map = {}
     self._cached_transitive_fingerprint_map = {}
+
+  @property
+  def tags(self):
+    return self._tags
 
   def assert_list(self, maybe_list, expected_type=Compatibility.string):
     return assert_list(maybe_list, expected_type, raise_type=lambda msg: TargetDefinitionException(self, msg))
