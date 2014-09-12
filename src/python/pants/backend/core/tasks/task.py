621e7e5953aea3d3e36b5a8a2014dbb298a057a6
David Taylor <davidtaylor@foursquare.com>
2014-09-12 05:14:14 +0000

    subproc cache write

--- src/python/pants/backend/core/tasks/task.py
+++ src/python/pants/backend/core/tasks/task.py
@@ -21,7 +21,7 @@ from pants.base.config import Config
 from pants.base.exceptions import TaskError
 from pants.base.worker_pool import Work
 from pants.base.workunit import WorkUnit
-from pants.cache.artifact_cache import call_use_cached_files
+from pants.cache.artifact_cache import call_insert, call_use_cached_files
 from pants.cache.cache_setup import create_artifact_cache
 from pants.cache.read_write_artifact_cache import ReadWriteArtifactCache
 from pants.reporting.reporting_utils import items_to_report_element
@@ -356,8 +356,11 @@ class TaskBase(AbstractClass):
       # Cache the artifacts.
       args_tuples = []
       for vts, artifactfiles in vts_artifactfiles_pairs:
-        args_tuples.append((vts.cache_key, artifactfiles))
-      return Work(lambda *args: cache.insert(*args), args_tuples, 'insert')
+        args_tuples.append((cache, vts.cache_key, artifactfiles))
+
+      def bg_insert(cache, key, files):
+        self.context.background_proc(call_insert, (cache, key, files))
+      return Work(bg_insert, args_tuples, 'insert')
     else:
       return None
 
